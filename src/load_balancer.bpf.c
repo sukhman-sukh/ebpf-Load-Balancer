d870101c7675d6b45751225894e19bf6cfa76c7c
sukhman <sukhmansinghsaluja@gmail.com>
2025-04-12 23:00:48 +0530

    Add TCP option and checksum validator

--- src/load_balancer.bpf.c
+++ src/load_balancer.bpf.c
@@ -40,6 +40,78 @@ struct
 	__uint(max_entries, MAX_TARGET_COUNT);
 } targets_map SEC(".maps");
 
+/**
+ * Map that holds the IP and MAC address of the load balancer and the client.
+ * The IP and MAC of the load balancer are in the first cell
+ * and the information for the client are in the second cell.
+ * This map is also populated when loading the XDP program.
+ */
+struct
+{
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__type(key, __u32);
+	__type(value, struct ip_mac_pair);
+	__uint(max_entries, MAX_TARGET_COUNT);
+} config_map SEC(".maps");
+
+/**
+ * Helper function for computing checksum
+ */
+static __always_inline __u16 csum_reduce_helper(__u32 csum)
+{
+	csum = ((csum & 0xffff0000) >> 16) + (csum & 0xffff);
+	csum = ((csum & 0xffff0000) >> 16) + (csum & 0xffff);
+
+	return csum;
+}
+
+/**
+ * Compute the checksum for tcp header
+ */
+static __always_inline int compute_tcp_csum(struct iphdr *ip, struct tcphdr *tcp, void *data_end)
+{
+	struct ipv4_psd_header psdh;
+	uint32_t csum;
+	int ret = 0;
+
+	tcp->check = 0;
+	csum = bpf_csum_diff(0, 0, (__be32 *)tcp, sizeof(struct tcphdr), 0);
+	psdh.src_addr = ip->saddr;
+	psdh.dst_addr = ip->daddr;
+	psdh.zero = 0;
+	psdh.proto = IPPROTO_TCP;
+	psdh.len = htons(ntohs(ip->tot_len) - sizeof(struct iphdr));
+	csum = bpf_csum_diff(0, 0, (__be32 *)&psdh, sizeof(struct ipv4_psd_header),
+						 csum);
+	uint32_t tcphdrlen = tcp->doff * 4;
+
+	if (tcphdrlen == sizeof(struct tcphdr))
+		goto OUT;
+
+	/* There are TCP options */
+	uint32_t *opt = (uint32_t *)(tcp + 1);
+	uint32_t parsed = sizeof(struct tcphdr);
+	for (int i = 0; i < MAX_OPT_WORDS; i++)
+	{
+		if ((void *)(opt + 1) > data_end)
+		{
+			ret = -1;
+			goto OUT;
+		}
+
+		csum = bpf_csum_diff(0, 0, (__be32 *)opt, sizeof(uint32_t), csum);
+
+		parsed += sizeof(uint32_t);
+		if (parsed == tcphdrlen)
+			break;
+		opt++;
+	}
+
+OUT:
+	tcp->check = ~csum_reduce_helper(csum);
+	return ret;
+}
+
 SEC("xdp")
 int xdp_lb(struct xdp_md *ctx)
 {
@@ -77,6 +149,19 @@ int xdp_lb(struct xdp_md *ctx)
 	ip->saddr = htonl(server_config->ip);
 	memcpy(&eth->h_source, server_config->mac.addr, 6);
 
+	/* Recalculate IP header checksum. */
+	ip->check = 0;
+	uint32_t csum = 0;
+	__u16 *next_iph_u16 = (__u16 *)ip;
+	for (int i = 0; i < sizeof(struct iphdr) / 2; i++)
+	{
+		csum += *next_iph_u16++;
+	}
+	ip->check = ~((csum & 0xffff) + (csum >> 16));
+
+	/* Recalculate TCP header checksum. */
+	compute_tcp_csum(ip, tcph, data_end);
+	
 	return XDP_TX;
 }
 
